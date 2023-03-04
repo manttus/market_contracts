fe449b52f4523f0a3224498bb3652fd30dd52649
Raymon <rbasnyat666@gmail.com>
2023-03-04 16:19:55 +0545

    update marketplace constructor to take contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,10 +6,10 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
   const NFT = await hre.ethers.getContractFactory("NFT");
   const nft = await NFT.deploy();
-  // const Marketplace = await hre.ethers.getContractFactory("Market");
-  // const marketplace = await Marketplace.deploy();
+  const Marketplace = await hre.ethers.getContractFactory("Market");
+  const marketplace = await Marketplace.deploy(nft.address);
   console.log("NFT address:", nft.address);
-  // console.log("Marketplace address:", marketplace.address);
+  console.log("Marketplace address:", marketplace.address);
 }
 
 main().catch((error) => {
